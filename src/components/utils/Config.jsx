dbab41df384b0c1fcbc1b0e270ec34481889db83
Monu MIshra <96140349+monumishra326@users.noreply.github.com>
2022-04-03 15:07:22 +0530

    Update Config.jsx

--- src/components/utils/Config.jsx
+++ src/components/utils/Config.jsx
@@ -2,15 +2,7 @@
 import firebase from "firebase";
 
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAjybY4pev4zMhbDRioGMETj_8I70h4gNQ",
-//   authDomain: "unacademy-clone-11a11.firebaseapp.com",
-//   projectId: "unacademy-clone-11a11",
-//   storageBucket: "unacademy-clone-11a11.appspot.com",
-//   messagingSenderId: "830572030637",
-//   appId: "1:830572030637:web:4ff4622053ed0b15927424",
-//   measurementId: "G-8Q27YGG4TR"
-// };
+
 const firebaseConfig = {
   apiKey: "AIzaSyAyBrIeJHLeoIIls59g3L0TSdScSkdii_0",
   authDomain: "unacdemy-clone-7f3b2.firebaseapp.com",
